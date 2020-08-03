ee9d30d94e3e96e2bf1e59df21d5e6f0abf7644d
phitranpt <phi.tran.pt@gmail.com>
2020-08-02 23:56:48 -0500

    updated landing page with picutre

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './LandingPage.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function LandingPage() {
   return (
-    <div className='App-Title'>
-      LandingPage
+    <div className='landing-page-container'>
+      <div className='background-img'>
+        <div className='social-media-icons'>
+          <FontAwesomeIcon 
+            className='facebook-icon'
+            icon={faFacebookSquare} 
+            size='2x' 
+          />
+          <FontAwesomeIcon 
+            className='instagram-icon'
+            icon={faInstagram} 
+            size='2x' 
+          />
+        </div>
+      </div>
+      <div className='logo-img'>
+        
+      </div>
     </div>
   );
 }
